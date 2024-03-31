6725f05401c138a0366559d5aaf79a954c1d03aa
liaz-repo <18819343569@139.com>
2024-03-31 14:56:06 +0800

    补充模版页面

---
+++ src/views/template.vue
@@ -0,0 +1,60 @@
+<template>
+    <section class="content">
+        <div class="box box-danger">
+            <div class="box-body">
+                <!-- Content Header (Page header) -->
+                <section class="content-header">
+                    <h1 id="itemTitle"></h1>
+                </section>
+                <!-- .content -->
+                <section class="content">
+                    <div id="table"></div>
+                </section><!-- .content -->
+            </div>
+        </div>
+    </section>
+</template>
+  
+<script>
+import TableHelper from '@/utils/bootstrap-table-helper';
+
+export default {
+    name: 'template',
+    data() {
+        return {
+            columns: [],
+        };
+    },
+    created() {
+        this.init();
+    },
+    methods: {
+        init() {
+            this.$nextTick(function () {
+                // jquery执行代码
+                let $this = this;
+                $this.initTable();
+            });
+        },
+        initTable() {
+            let $this = this;
+            TableHelper.destroy('#table');
+            $('#table').bootstrapTable({
+                columns: $this.columns,
+                cache: false,
+                striped: true,
+                showRefresh: true,
+                search: true,
+                pageSize: 10,
+                pagination: true,
+                pageList: [1, 10, 20, 30, 50],
+                sidePagination: "client",
+                queryParamsType: "undefined",
+                toolbar: '#toolbar',
+            });
+        },
+    },
+}
+</script>
+  
+<style scoped></style>
